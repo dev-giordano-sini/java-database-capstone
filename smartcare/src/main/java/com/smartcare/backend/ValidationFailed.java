f088517eea0523e7820efba879b0f8bbd4c46683
Giordano Sini <giordano81@gmail.com>
2026-05-22 18:00:07 +0200

    wip

--- smartcare/src/main/java/com/smartcare/backend/ValidationFailed.java
+++ smartcare/src/main/java/com/smartcare/backend/ValidationFailed.java
@@ -3,12 +3,32 @@ package com.smartcare.backend;
 
 import org.apache.commons.logging.LogFactory;
 import org.apache.juli.logging.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ValidationFailed {
 
     private final Log log = (Log) LogFactory.getLog(this.getClass());
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class
+    })
+    public ResponseEntity<String> handle(MethodArgumentNotValidException e){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        return  new ResponseEntity<>(errors.getFirst(), status);
+    }
 }
